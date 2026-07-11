2e472e76b401e32f4cdc13e886062f689353eb42
hitizo <hizzt56@gmail.com>
2026-07-11 20:57:42 -0300

    NEW NAVBAR SYSTEM!!

---
+++ nav.js
@@ -0,0 +1,13 @@
+document.getElementById("navBar").innerHTML = `
+                    on site<br>
+                    ↳ <a href="home.html">home</a><br>
+                    ↳ <a href="profile.html">profile</a><br>
+                    ↳ <a href="blog.html">blog</a><br>
+                    ↳ <a href="projects.html">projects</a><br>
+                    ↳ <a href="guestbook.html">guestbook</a><br>
+                    ↳ <a href="credits.html">credits</a><br><br>
+                    <br>
+
+                    off site<br>
+                    ↳ <a href="https://neocities.org/site/hitizo">neocities</a><br></br>
+`;
