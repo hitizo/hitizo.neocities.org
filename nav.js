36a33c8bc92c5e59e24b3ace9122e77234ff8e6c
hitizo <hizzt56@gmail.com>
2026-07-15 10:29:23 -0300

    fuck fonts

--- nav.js
+++ nav.js
@@ -3,6 +3,7 @@ document.getElementById("navBar").innerHTML = `
                     ↳ <a href="home.html">home</a><br>
                     ↳ <a href="profile.html">profile</a><br>
                     ↳ <a href="blog.html">blog</a><br>
+                    ↳ <a href="">shrines</a><br>
                     ↳ <a href="projects.html">projects</a><br>
                     ↳ <a href="credits.html">credits</a><br><br>
                     <br>
@@ -11,4 +12,10 @@ document.getElementById("navBar").innerHTML = `
                     ↳ <a href="https://hitizo.atabook.org">guestbook</a><br>
                     ↳ <a href="https://neocities.org/site/hitizo">neocities</a><br></br>
 
+`;
+
+document.getElementById("footer").innerHTML = `
+            coded with <3 | v2.0 | hitizo.neocities.org © 2024 - ENERNITY <br><br>
+            *yeah no copyrights or anything, everything here is free to use <br>
+            **if i'm using anything that's yours and you want me to remove it, just contact me and i'll remove it
 `;
